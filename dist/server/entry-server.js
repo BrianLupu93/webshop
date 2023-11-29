94106e6ac807a30ae1e753d5957b2f957ec9ef30
BrianLupu93 <brianlupu@gmail.com>
2023-11-29 15:18:18 +0100

    react icons

--- dist/server/entry-server.js
+++ dist/server/entry-server.js
@@ -1,8 +1,31 @@
-import { jsx, Fragment } from "react/jsx-runtime";
+import { jsxs, jsx, Fragment } from "react/jsx-runtime";
 import React from "react";
 import ReactDOMServer from "react-dom/server";
+const BannerSmall = ({
+  color,
+  title,
+  text
+}) => {
+  return /* @__PURE__ */ jsxs(
+    "div",
+    {
+      className: `bg-${color} flex flex-col justify-center items-center height:20`,
+      children: [
+        /* @__PURE__ */ jsx("div", { className: "text-md font-bold", children: title }),
+        /* @__PURE__ */ jsx("div", { className: "text-sm", children: text })
+      ]
+    }
+  );
+};
 function App() {
-  return /* @__PURE__ */ jsx(Fragment, { children: /* @__PURE__ */ jsx("div", { children: "test" }) });
+  return /* @__PURE__ */ jsx(Fragment, { children: /* @__PURE__ */ jsx(
+    BannerSmall,
+    {
+      title: "Black friday",
+      text: "Up to 60% black discounts",
+      color: "red-500"
+    }
+  ) });
 }
 function render() {
   const html = ReactDOMServer.renderToString(
